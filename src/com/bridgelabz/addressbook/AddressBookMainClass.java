baac3107208ef7aa698452cd1cd1e7ae1d5d6819
Textspot <sachinkatarki1995@gmail.com>
2021-11-29 15:24:35 +0530

    Added:Creat a First and Last Name Phone number in Adress book

--- src/com/bridgelabz/addressbook/AddressBookMainClass.java
+++ src/com/bridgelabz/addressbook/AddressBookMainClass.java
@@ -1,5 +1,9 @@
 package com.bridgelabz.addressbook;
 
 public class AddressBookMainClass {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book  Program");
+
+	}
 
 }
